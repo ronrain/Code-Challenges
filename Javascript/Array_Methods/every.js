4a7af79afe7c41114b9eee8fd7b18f22cb5dcfd3
RonRain <rarainero@gmail.com>
2023-11-13 16:41:57 -0500

    every

---
+++ Javascript/Array_Methods/every.js
@@ -0,0 +1,4 @@
+const numbers = [1, 2, 3, 4];
+const allPositive = numbers.every((num) => num > 0);
+// allPositive is true >>>>> all greater than zero
+// checks if all the elemnts in the array satify provided testing function
