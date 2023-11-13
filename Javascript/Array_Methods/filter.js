c64abb737b6148d20cba9334967f7d616e70e0bd
RonRain <rarainero@gmail.com>
2023-11-13 16:34:13 -0500

    filter

---
+++ Javascript/Array_Methods/filter.js
@@ -0,0 +1,4 @@
+const numbers = [1, 2, 3, 4];
+const evenNumbers = numbers.filter((num) => num % 2 === 0);
+// evenNumbers is [2, 4]
+//creates a new array for the elements that pass the test provided by the function
