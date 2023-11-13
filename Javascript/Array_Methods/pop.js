8d25ccd328ed4d0afd21021e19f318036262050b
RonRain <rarainero@gmail.com>
2023-11-13 16:00:33 -0500

    push

---
+++ Javascript/Array_Methods/pop.js
@@ -0,0 +1,4 @@
+const fruits = ['apple', 'banana', 'orange'];
+const removedFruit = fruits.pop();
+// removedFruit is 'orange', and fruits is now ['apple', 'banana']
+//removes the last element in the array and returns it
