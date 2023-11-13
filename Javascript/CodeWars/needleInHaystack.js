785e50ef8b85bac4a0c2cbd35c9f5fe96e49d421
RonRain <rarainero@gmail.com>
2023-11-13 15:57:36 -0500

    haystack

---
+++ Javascript/CodeWars/needleInHaystack.js
@@ -0,0 +1,6 @@
+function findNeedle(haystack) {
+  for(let i = 0; i < haystack.length; i++){//I do not need the loop
+    haystack.indexOf('needle')
+  }
+  return (`found the needle at position ${haystack.indexOf('needle')}`)// works with this
+}
