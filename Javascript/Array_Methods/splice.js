b5edbac2f1cd4a4126c9586f0543401854cced8e
RonRain <rarainero@gmail.com>
2023-11-13 16:17:45 -0500

    splice

---
+++ Javascript/Array_Methods/splice.js
@@ -0,0 +1,5 @@
+const fruits = ['apple', 'banana', 'orange', 'kiwi'];
+fruits.splice(1, 2, 'grape', 'pear');
+// fruits is now ['apple', 'grape', 'pear', 'kiwi']
+// replaces the elements with the new elemnts identified 
+//indeces selected are the indeces replaced
