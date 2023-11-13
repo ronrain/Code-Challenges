f5ee090535dc6aa975d58598c040af2bd46f247e
RonRain <rarainero@gmail.com>
2023-11-13 16:09:46 -0500

    concat

---
+++ Javascript/Array_Methods/concat.js
@@ -0,0 +1,5 @@
+const fruits1 = ['apple', 'banana'];
+const fruits2 = ['orange', 'kiwi'];
+const combinedFruits = fruits1.concat(fruits2);
+// combinedFruits is ['apple', 'banana', 'orange', 'kiwi']
+//combines 2 or more arrays and returns a new one
