20c36002799c21907fd136ff93443cc71a8f4bce
RonRain <rarainero@gmail.com>
2023-11-13 16:14:34 -0500

    slice

---
+++ Javascript/Array_Methods/slice.js
@@ -0,0 +1,5 @@
+const fruits = ['apple', 'banana', 'orange', 'kiwi'];
+const slicedFruits = fruits.slice(1, 3);
+// slicedFruits is ['banana', 'orange']
+//starts at the index identified and ends at the index identified
+//returns the elemnts selected
