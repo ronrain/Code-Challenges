4dea7799dabe616e9861890b8b53cab3fcd209a9
RonRain <rarainero@gmail.com>
2023-11-13 16:22:49 -0500

    lastIndexOf

---
+++ Javascript/Array_Methods/lastIndexOf.js
@@ -0,0 +1,4 @@
+const fruits = ['apple', 'banana', 'orange', 'kiwi', 'banana'];
+const lastIndex = fruits.lastIndexOf('banana');
+// lastIndex is 4
+//returns the last in dex a specified element can be found in an array
