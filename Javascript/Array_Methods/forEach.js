e8c4d6d0fdf1e0249ba0a5fbb3e6bc0f70829115
RonRain <rarainero@gmail.com>
2023-11-13 16:32:09 -0500

    map

---
+++ Javascript/Array_Methods/forEach.js
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3, 4];
+numbers.forEach((num) => {
+  console.log(num);
+});
+// Output: 1, 2, 3, 4
+//calls a provided function once for each element in an array
---
+++ Javascript/Array_Methods/map.js
@@ -0,0 +1,4 @@
+const numbers = [1, 2, 3, 4];
+const squaredNumbers = numbers.map((num) => num * num);
+// squaredNumbers is [1, 4, 9, 16]
+// creates a new array with the results of calling a provided function on every element in the array
