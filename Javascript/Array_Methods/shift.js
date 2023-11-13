e34a8f8ef757cbf0fdb3db8d2cbf7c385b9824c3
RonRain <rarainero@gmail.com>
2023-11-13 16:02:38 -0500

    shift

---
+++ Javascript/Array_Methods/shift.js
@@ -0,0 +1,4 @@
+const fruits = ['apple', 'kiwi', 'banana', 'orange'];
+const removedFruit = fruits.shift();
+// removedFruit is 'apple', and fruits is now ['kiwi', 'banana', 'orange']
+//removes first element in the array
