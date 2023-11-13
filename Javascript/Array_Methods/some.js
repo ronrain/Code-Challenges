46f198375fd608304af5bc6905d020e0229342ba
RonRain <rarainero@gmail.com>
2023-11-13 16:40:45 -0500

    some

---
+++ Javascript/Array_Methods/some.js
@@ -0,0 +1,4 @@
+const numbers = [1, 2, 3, 4];
+const hasEvenNumber = numbers.some((num) => num % 2 === 0);
+// hasEvenNumber is true >>> 2 and 4
+//checks if at least one item in the array satifies the provided testing function
