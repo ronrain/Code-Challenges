8ebe957d36d801ee9b5859f3bcc8a9cdf5884962
RonRain <rarainero@gmail.com>
2023-12-26 14:35:56 -0500

    reverese order

---
+++ Python/CodeAcademy/reverse_list.py
@@ -0,0 +1,6 @@
+def reversed_list(lst1, lst2):
+  for index in range(len(lst1)):#loops through indicies of lst1
+    #goes from 0 to the length, have to add the -1 to match index number
+    if lst1[index] != lst2[len(lst2) - 1 - index]: # checks if the element at the current index is not equal to the aleemnt at the corresponding position when counting to the end of lst2. lst2[len(lst2) - 1 - index] retrieves the element from lst2 at the position that iw ould correspond in reverse order
+      return False
+  return True
