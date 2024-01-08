1b8c3abd05d12ca73f7159dafbefd4e3237e633b
RonRain <rarainero@gmail.com>
2024-01-08 10:17:51 -0500

    player to points calculation

--- Python/CodeAcademy/scrabble.py
+++ Python/CodeAcademy/scrabble.py
@@ -3,7 +3,7 @@ points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 
 
 letter_to_point = dict(zip(letters, points))
 letter_to_point[" "] = 0
-print(letter_to_point)
+# print(letter_to_point)
 
 def score_word(word):
   point_total = 0
@@ -13,6 +13,18 @@ def score_word(word):
       point_total += 0
   return point_total
 
-print(score_word("BROWNIE"))
+# print(score_word("BROWNIE"))
 
-player_to_word = {"Player 1": ["BLUE", "TENNIS", "EXIT"], "Lexi Con": ["ERASER", "BELLY", "HUSKY"], "Prof Reader": ["ZAP", "COMA", "PERIOD"]}
+player_to_words = {"Player 1": ["BLUE", "TENNIS", "EXIT"], "Word Nerd": ["EARTH", "EYES", "MACHINE"], "Lexi Con": ["ERASER", "BELLY", "HUSKY"], "Prof Reader": ["ZAP", "COMA", "PERIOD"]}
+
+player_to_points = {}
+for player, words in player_to_words.items():
+  player_points = 0
+  for word in words:
+    player_points += score_word(word)
+  player_to_points[player] = player_points
+
+print(player_to_points)
+
+
+
