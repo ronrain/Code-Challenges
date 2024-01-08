4547957f9e4a2bdf5d05656f446dbfa2bbf9c607
RonRain <rarainero@gmail.com>
2024-01-08 10:54:32 -0500

    handle lower case input

--- Python/CodeAcademy/scrabble.py
+++ Python/CodeAcademy/scrabble.py
@@ -1,15 +1,26 @@
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
-letter_to_point = dict(zip(letters, points))
-letter_to_point[" "] = 0
+letters += [
+  letter.lower()
+  for letter
+  in letters
+]
+points *= 2
+letter_to_points = {
+  key:value
+  for key, value
+  in zip(letters, points)
+}
+
+letter_to_points[" "] = 0
 # print(letter_to_point)
 
 def score_word(word):
   point_total = 0
   for letter in word:
-    point_total += letter_to_point[letter]
-    if letter not in letter_to_point:
+    point_total += letter_to_points[letter]
+    if letter not in letter_to_points:
       point_total += 0
   return point_total
 
