30ebb6e745f2616f28b468ed8b0950bda19dfa24
RonRain <rarainero@gmail.com>
2024-01-08 09:53:51 -0500

    create function to track number of points for word input

--- Python/CodeAcademy/scrabble.py
+++ Python/CodeAcademy/scrabble.py
@@ -4,3 +4,13 @@ points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 
 letter_to_point = dict(zip(letters, points))
 letter_to_point[" "] = 0
 print(letter_to_point)
+
+def score_word(word):
+  point_total = 0
+  for letter in word:
+    point_total += letter_to_point[letter]
+    if letter not in letter_to_point:
+      point_total += 0
+  return point_total
+
+print(score_word("BROWNIE"))
